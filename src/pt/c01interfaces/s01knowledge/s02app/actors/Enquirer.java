a7b485c4e9a713fa0142f13be936455ab426bd52
J146584 <j146584@dac.unicamp.com>
2015-03-10 14:53:30 -0300

    Update Enquirer.java

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -15,35 +15,68 @@ public class Enquirer implements IEnquirer
 	{
 	}
 	
-	
-	@Override
+
 	public void connect(IResponder responder)
 	{
         IBaseConhecimento bc = new BaseConhecimento();
-		
-		obj = bc.recuperaObjeto("tiranossauro");
-
+        
+        int i = 0, j = 0, k = 0, ja = 0;
+        
+        String[] bichos = bc.listaNomes();
+        String[] perjafeita = new String[42];
+        String[] respJaFeita = new String[42];
+		obj = bc.recuperaObjeto(bichos[i]);
 		IDeclaracao decl = obj.primeira();
+		decl = obj.primeira();
 		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
-			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
+	while (decl != null) {
+
+		String pergunta = decl.getPropriedade();
+		String respostaEsperada = decl.getValor();
+		
+		if(k != 0){											//temos alguma resposta ja feita
+			while(perjafeita[j] != null && ja == 0){
+				if(pergunta.equalsIgnoreCase(perjafeita[j])){
+					ja = 1;
+				}else{
+					j++;
+				}
+			}
 		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
-		
-		if (acertei)
-			System.out.println("Oba! Acertei!");
-		else
-			System.out.println("fuem! fuem! fuem!");
+			j=0;
+		    if(ja==1){										//pergunta ja feita
+		    	if (respJaFeita[j].equalsIgnoreCase(respostaEsperada))		//compara respostaEsperada com resposta dada
+					decl = obj.proxima();								// Se igual próximo
+				else{
+					i++;
+					obj = bc.recuperaObjeto(bichos[i]);				//prox animal
+					decl = obj.primeira();
+				}
+		    	ja=0;
+		    }else{		//pergunta ainda não feita
+		    	String resposta = responder.ask(pergunta);
+		    	respJaFeita[j] = resposta;
+		    	perjafeita[j] = pergunta;
+		    	j++;
+		    	if (resposta.equalsIgnoreCase(respostaEsperada))		//compara respostaEsperada com resposta dada
+					decl = obj.proxima();								// Se igual próximo
+				else{
+					i++;
+					obj = bc.recuperaObjeto(bichos[i]);				//prox animal
+					decl = obj.primeira();
+				}
+		    }
+		    k++;
+		}
+       										
+	boolean acertei = responder.finalAnswer(bichos[i]);
+	if (acertei)
+		System.out.println("Oba! Acertei!");
+	else
+		System.out.println("fuem! fuem! fuem!");
 
 	}
+  
 
 }
+
